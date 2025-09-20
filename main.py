2ddfe516dea6034ce29c614833e3f316ce7afd8b
BolderDev <drshraddhaji@gmail.com>
2025-09-20 18:26:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 kbc=["who is the PM of India? : ", "Who is the President of India? : ", "who is nations bird of India? : ", "who is national animal of India? : ","In which language Microsoft OS is written? : ", "who is known as iron man of India? : ","who is known as father of India? : "]
-answer=["Narendra Modi", "Dropadi Murmu", "Peacock", "Tiger","c++","Vallabh Bhai Patel","Mahatma Gandu"]
+answer=["Narendra Modi", "Dropadi Murmu", "Peacock", "Tiger","c++","Vallabh Bhai Patel","Mahatma Gandhi"]
 amount=[1000,3000,5000,10000,40000,100000,320000]
 winningamt=[0,0,0,10000,10000,10000,320000]
 
